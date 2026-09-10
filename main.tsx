0b94d0666d7c2a26d98c5cde03af98d81fb4c99b
pbsistemaspastobom-code <pbsistemaspastobom@gmail.com>
2026-09-10 10:40:37 -0300

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -3,3 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 createRoot(document.getElementById("root")!).render(<React.StrictMode><App /></React.StrictMode>);
+
+// Registra o service worker (habilita "Instalar app" no navegador)
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(() => { /* segue sem PWA se falhar */ });
+  });
+}
